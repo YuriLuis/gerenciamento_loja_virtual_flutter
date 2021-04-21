4e34dd0c2884a0f9c6856fc2f77023ab6e09155c
yuri.pereira <yuri.pereira@ambevtech.com.br>
2021-04-21 15:37:36 -0300

    Aula 251 Bloqueando a tela durante o salvemento.

--- lib/blocs/products_bloc.dart
+++ lib/blocs/products_bloc.dart
@@ -1,11 +1,16 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:rxdart/rxdart.dart';
 
 class ProductBloc extends BlocBase {
   final _dataController = BehaviorSubject<Map>();
+  final _loadingController = BehaviorSubject<bool>();
+  final _createdController = BehaviorSubject<bool>();
 
   Stream<Map> get outData => _dataController.stream;
+  Stream<bool> get outLoading => _loadingController.stream;
+  Stream<bool> get outCreated => _createdController.stream;
   String categoryId;
   DocumentSnapshot product;
   Map<String, dynamic> unsavedData;
@@ -45,5 +50,43 @@ class ProductBloc extends BlocBase {
   @override
   void dispose() {
     _dataController.close();
+  }
+
+  Future<bool> saveProduct() async {
+    _loadingController.add(true);
+
+    try {
+      if(product != null){
+        await _uploadImages(product.documentID);
+        await product.reference.updateData(unsavedData);
+      } else {
+        DocumentReference dr = await Firestore.instance.collection("products").document(categoryId).
+        collection("items").add(Map.from(unsavedData)..remove("images"));
+        await _uploadImages(dr.documentID);
+        await dr.updateData(unsavedData);
+      }
+
+      _createdController.add(true);
+      _loadingController.add(false);
+      return true;
+    } catch (e){
+      _loadingController.add(false);
+      return false;
+    }
+  }
+
+  Future _uploadImages(String productId) async {
+    for(int i = 0; i < unsavedData["images"].length; i++){
+      if(unsavedData["images"][i] is String) continue;
+
+      StorageUploadTask uploadTask = FirebaseStorage.instance.ref().child(categoryId).
+      child(productId).child(DateTime.now().millisecondsSinceEpoch.toString()).
+      putFile(unsavedData["images"][i]);
+
+      StorageTaskSnapshot s = await uploadTask.onComplete;
+      String downloadUrl = await s.ref.getDownloadURL();
+
+      unsavedData["images"][i] = downloadUrl;
+    }
   }
 }
--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -14,31 +14,32 @@ class ProductScreen extends StatefulWidget {
   _ProductScreenState createState() => _ProductScreenState(categoryId, product);
 }
 
-class _ProductScreenState extends State<ProductScreen> with ProductValidator{
+class _ProductScreenState extends State<ProductScreen> with ProductValidator {
   final ProductBloc _productBloc;
   final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   _ProductScreenState(String categoryId, DocumentSnapshot product)
       : _productBloc = ProductBloc(categoryId: categoryId, product: product);
 
   @override
   Widget build(BuildContext context) {
-    final _fieldStyle =
-        TextStyle(color: Colors.white, fontSize: 16);
+    final _fieldStyle = TextStyle(color: Colors.white, fontSize: 16);
 
     InputDecoration _buildDecoration(String label) {
       return InputDecoration(
         labelText: label,
         labelStyle: TextStyle(color: Colors.white),
-        enabledBorder:
-            UnderlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(255, 199, 44, 1.0))),
-        focusedBorder:
-            UnderlineInputBorder(borderSide: BorderSide(color: Color.fromRGBO(255, 199, 44, 1.0))),
+        enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Color.fromRGBO(255, 199, 44, 1.0))),
+        focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Color.fromRGBO(255, 199, 44, 1.0))),
         hintStyle: TextStyle(color: Colors.green),
       );
     }
 
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: Color.fromRGBO(45, 42, 38, 1.0),
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(255, 199, 44, 1.0),
@@ -46,66 +47,115 @@ class _ProductScreenState extends State<ProductScreen> with ProductValidator{
         title: Text('Criar Produto'),
         actions: [
           IconButton(icon: Icon(Icons.remove), onPressed: () {}),
-          IconButton(icon: Icon(Icons.save), onPressed: () {
-            if(_formKey.currentState.validate()){
-              _formKey.currentState.save();
-            }
-          })
+          StreamBuilder<bool>(
+              stream: _productBloc.outLoading,
+              initialData: false,
+              builder: (context, snapshot) {
+                return IconButton(
+                    icon: Icon(Icons.save),
+                    onPressed: snapshot.data ? null : saveProduct);
+              })
         ],
       ),
-      body: Form(
-        key: _formKey,
-        child: StreamBuilder<Map>(
-          stream: _productBloc.outData,
-          builder: (context, snapshot) {
-            if(!snapshot.hasData){
-              return Container();
-            }else {
-              return ListView(
-                padding: EdgeInsets.all(16),
-                children: [
-                  Text('Imagens', style: TextStyle(color: Colors.white, fontSize: 12),),
-                  ImagesWidget(context: context,
-                  initialValue: snapshot.data['images'],
-                  onSaved: _productBloc.saveImages,
-                  // ignore: missing_return
-                  validator: validateImages,),
-                  TextFormField(
-                    initialValue: snapshot.data['title'],
-                    cursorColor: Color.fromRGBO(255, 199, 44, 1.0),
-                    style: _fieldStyle,
-                    decoration: _buildDecoration('titulo'),
-                    onSaved: _productBloc.saveTitle,
-                    // ignore: missing_return
-                    validator: validateTitle,
+      body: Stack(
+        children: [
+          Form(
+            key: _formKey,
+            child: StreamBuilder<Map>(
+                stream: _productBloc.outData,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Container();
+                  } else {
+                    return ListView(
+                      padding: EdgeInsets.all(16),
+                      children: [
+                        Text(
+                          'Imagens',
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        ),
+                        ImagesWidget(
+                          context: context,
+                          initialValue: snapshot.data['images'],
+                          onSaved: _productBloc.saveImages,
+                          // ignore: missing_return
+                          validator: validateImages,
+                        ),
+                        TextFormField(
+                          initialValue: snapshot.data['title'],
+                          cursorColor: Color.fromRGBO(255, 199, 44, 1.0),
+                          style: _fieldStyle,
+                          decoration: _buildDecoration('titulo'),
+                          onSaved: _productBloc.saveTitle,
+                          // ignore: missing_return
+                          validator: validateTitle,
+                        ),
+                        TextFormField(
+                          initialValue: snapshot.data['description'],
+                          cursorColor: Color.fromRGBO(255, 199, 44, 1.0),
+                          style: _fieldStyle,
+                          maxLines: 6,
+                          decoration: _buildDecoration('Descrição'),
+                          onSaved: _productBloc.saveDescription,
+                          // ignore: missing_return
+                          validator: validateDescription,
+                        ),
+                        TextFormField(
+                          initialValue: snapshot.data['price']?.toStringAsFixed(2),
+                          cursorColor: Color.fromRGBO(255, 199, 44, 1.0),
+                          style: _fieldStyle,
+                          decoration: _buildDecoration('Preço'),
+                          keyboardType:
+                              TextInputType.numberWithOptions(decimal: true),
+                          onSaved: _productBloc.savePrice,
+                          // ignore: missing_return
+                          validator: validatePrice,
+                        )
+                      ],
+                    );
+                  }
+                }),
+          ),
+          StreamBuilder<bool>(
+              stream: _productBloc.outLoading,
+              initialData: false,
+              builder: (context, snapshot) {
+                return IgnorePointer(
+                  ignoring: !snapshot.data,
+                  child: Container(
+                    color: snapshot.data ? Colors.black54 : Colors.transparent,
                   ),
-                  TextFormField(
-                    initialValue: snapshot.data['description'],
-                    cursorColor: Color.fromRGBO(255, 199, 44, 1.0),
-                    style: _fieldStyle,
-                    maxLines: 6,
-                    decoration: _buildDecoration('Descrição'),
-                    onSaved: _productBloc.saveDescription,
-                    // ignore: missing_return
-                    validator: validateDescription,
-                  ),
-                  TextFormField(
-                    initialValue: snapshot.data['price']?.toStringAsFixed(2),
-                    cursorColor: Color.fromRGBO(255, 199, 44, 1.0),
-                    style: _fieldStyle,
-                    decoration: _buildDecoration('Preço'),
-                    keyboardType: TextInputType.numberWithOptions(decimal: true),
-                    onSaved: _productBloc.savePrice,
-                    // ignore: missing_return
-                    validator: validatePrice,
-                  )
-                ],
-              );
-            }
-
-          }
-        ),
+                );
+              })
+        ],
       ),
     );
+  }
+
+  void saveProduct() async {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        content: Text(
+          "Salvando produto...",
+          style: TextStyle(color: Colors.white),
+        ),
+        duration: Duration(minutes: 1),
+        backgroundColor: Color.fromRGBO(255, 199, 44, 1.0),
+      ));
+
+      bool success = await _productBloc.saveProduct();
+
+      _scaffoldKey.currentState.removeCurrentSnackBar();
+
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        content: Text(
+          success ? "Produto salvo!" : "Erro ao salvar produto!",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color.fromRGBO(255, 199, 44, 1.0),
+      ));
+    }
   }
 }
